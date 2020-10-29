c04a88b65cd1954c30cc181528c7a1ea1c2bbef9
Nick LS Whelan <Nicholas.LS.Whelan@gmail.com>
2020-10-28 17:12:00 -0700

    partially replace the RegEx PuncCap function with regular expressions.

--- js/scriptsRE.js
+++ js/scriptsRE.js
@@ -9,7 +9,6 @@
 
 //1.Global Variables
 
-const symbols = [".", ",", "!", "?", ":", ";", "\""]
 let quote = false;
 
 //2.Consonant Function
@@ -22,25 +21,24 @@ function consonant(word) {
 }
 
 //3.PuncCap Function
-//Moves end-of-word punctuation where it belongs after all letter shuffling is complete, and checks to see if the word was capitalized, then re-capitalizes the new first letter.
+//Starts by rearranging all words so that any punctuation in them (except an apostrophe or underscore) is moved to the end.
+//If the word contains a capital letter, the whole word is made lower case, then the first letter is capitalized. 
 
-function puncCap(word){
-  let reorder = word.split("");
-  const x = reorder.length;
-  for (i = 0; i < x; i+=1) {
-    if (symbols.includes(reorder[i])){
-      reorder.push(reorder[i]);
-      delete reorder[i];
-    } else if (reorder[i] === reorder[i].toUpperCase() && reorder[i] !== "\'"){
-      reorder[i] = reorder[i].toLowerCase();
-      reorder[0] = reorder[0].toUpperCase();
-    } 
-  }
-  if (quote === true){
+// NEEDS TO FIX QUOTATIONS AS WELL, DOESN'T DO THAT!
+/*Old, unfuctional code:
+if (quote === true){
     reorder.unshift("\"");
     quote = false;
   }
-  return reorder.join("");
+*/
+
+function puncCap(word){
+  word = word.replace(/(\w*)([^A-Za-z0-9_'])(\w*)/i, "$1$3$2");
+  if (word.match(/[A-Z]/)){
+    word = word.toLowerCase();
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+  return word;
 }
 
 //4.PigLatin Function
